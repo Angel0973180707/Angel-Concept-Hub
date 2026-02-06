4352babdb756861dfcca4040e7ac8149b52a07d5
Angel0973180707 <a0973180707@gmail.com>
2026-02-06 08:58:13 +0800

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,38 +1,4 @@
-const CACHE_NAME = "angel-concept-hub-v1";
-const ASSETS = [
-  "./",
-  "./index.html",
-  "./styles.css",
-  "./app.js",
-  "./manifest.json",
-  "./icons/icon-192.png",
-  "./icons/icon-512.png"
-];
 
-self.addEventListener("install", (event) => {
-  event.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS)).then(() => self.skipWaiting())
-  );
-});
-
-self.addEventListener("activate", (event) => {
-  event.waitUntil(
-    caches.keys().then((keys) =>
-      Promise.all(keys.map((k) => (k !== CACHE_NAME ? caches.delete(k) : null)))
-    ).then(() => self.clients.claim())
-  );
-});
-
-self.addEventListener("fetch", (event) => {
-  const req = event.request;
-  event.respondWith(
-    caches.match(req).then((cached) => {
-      if (cached) return cached;
-      return fetch(req).then((res) => {
-        const copy = res.clone();
-        caches.open(CACHE_NAME).then((cache) => cache.put(req, copy));
-        return res;
-      }).catch(() => caches.match("./index.html"));
-    })
-  );
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open('angel-hub').then(c => c.addAll(['./'])));
 });
