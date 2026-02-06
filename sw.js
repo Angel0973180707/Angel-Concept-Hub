8c401a90c352e73a1150731ace3fb1a950afd5c6
Angel0973180707 <a0973180707@gmail.com>
2026-02-06 09:09:24 +0800

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,41 @@
+const CACHE_NAME = 'angel-hub-v2'; // ← 重點：版本一定要換
 
-self.addEventListener('install', e => {
-  e.waitUntil(caches.open('angel-hub').then(c => c.addAll(['./'])));
+const ASSETS = [
+  './',
+  './index.html',
+  './styles.css',
+  './app.js',
+  './manifest.json',
+  './icons/icon-192.png',
+  './icons/icon-512.png'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(ASSETS))
+      .then(() => self.skipWaiting())
+  );
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(
+        keys.map(key => {
+          if (key !== CACHE_NAME) {
+            return caches.delete(key); // 🔥 清掉舊版
+          }
+        })
+      )
+    ).then(() => self.clients.claim())
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(res => {
+      return res || fetch(event.request);
+    })
+  );
 });
